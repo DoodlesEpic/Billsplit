935b964532c3f8289ee538826792dc5553b79abc
Doodles <moraes.eduardo@proton.me>
2023-12-31 18:51:18 -0300

    feat: generate service worker through vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
 		sveltekit(),
 		VitePWA({
 			registerType: 'autoUpdate',
+			strategies: 'generateSW',
 			manifest: {
 				name: 'Billsplit',
 				short_name: 'Billsplit',
