f0b08d9c66f16610fce44d992822ab58db86f798
Doodles <moraes.eduardo@proton.me>
2023-12-31 02:40:57 -0300

    chore: use only vite-plugin-pwa, for autoUpdate
    According to the docs at https://vite-pwa-org.netlify.app/frameworks/ it
    is not necessary to use the SvelteKit framework module if we are using
    autoUpdate, which would be the preferred strategy.
    
    Last commit is not reverted because it adds the manifest to the
    svelte:head through the layout file, according to the docs at
    https://vite-pwa-org.netlify.app/frameworks/

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA()]
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			registerType: 'autoUpdate'
+		})
+	]
 });
