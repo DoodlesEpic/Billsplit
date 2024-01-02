1333bb8eaf0b54ec4cd385dd06607b938751a4ec
Doodles <moraes.eduardo@proton.me>
2024-01-01 23:40:54 -0300

    feat: add a side drawer for navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
+	import Drawer, { Content, Scrim } from '@smui/drawer';
+	import List, { Item, Text, Graphic, Separator } from '@smui/list';
+	import IconButton from '@smui/icon-button';
+	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import { pwaInfo } from 'virtual:pwa-info';
 
@@ -19,16 +23,45 @@
 	});
 
 	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+
 	let topAppBar: TopAppBar;
+	let open = false;
+
+	const toggleDrawer = () => (open = !open);
 </script>
 
 <svelte:head>
 	{@html webManifestLink}
 </svelte:head>
 
+<Drawer variant="modal" fixed={false} bind:open>
+	<Row>
+		<Section>
+			<IconButton class="material-icons" on:click={toggleDrawer}>menu</IconButton>
+			<Title>Billsplit</Title>
+		</Section>
+	</Row>
+	<Separator />
+	<Content>
+		<List>
+			<Item href="/" on:click={toggleDrawer} activated={$page.url.pathname === '/'}>
+				<Graphic class="material-icons" aria-hidden="true">receipt</Graphic>
+				<Text>Bill</Text>
+			</Item>
+			<Item href="/about" on:click={toggleDrawer} activated={$page.url.pathname === '/about'}>
+				<Graphic class="material-icons" aria-hidden="true">info</Graphic>
+				<Text>About</Text>
+			</Item>
+		</List>
+	</Content>
+</Drawer>
+
+<Scrim fixed={false} />
+
 <TopAppBar bind:this={topAppBar} variant="fixed">
 	<Row>
 		<Section>
+			<IconButton class="material-icons" on:click={toggleDrawer}>menu</IconButton>
 			<Title>Billsplit</Title>
 		</Section>
 	</Row>
