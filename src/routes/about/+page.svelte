57feb560e27f9bba2360a0a9183bcac165e38c77
Doodles <moraes.eduardo@proton.me>
2024-01-01 23:40:43 -0300

    feat: add an about page with info

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<main style="margin: auto; padding: 1rem; max-width: 800px;">
+	<h2 style="margin-bottom: 2rem">About Billsplit</h2>
+	<p>
+		<b>Billsplit</b> is an easy and fast way to split restaurant bills. It's a simple progressive web
+		application built with SvelteKit that helps separate costs between people. The application does not
+		store your data, nor collect usage analytics.
+	</p>
+	<p>
+		This is free software, licensed under the GNU Affero General Public License v3. It's developed
+		by
+		<a href="https://www.doodlesdev.com" target="_blank">Doodles</a>, and the source code is
+		available on <a href="https://github.com/DoodlesEpic/Billsplit">GitHub</a> and
+		<a href="https://gitlab.com/doodlesdev/Billsplit">Gitlab</a>.
+	</p>
+</main>
